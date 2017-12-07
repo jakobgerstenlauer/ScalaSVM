83066434ed39f121a6d4e6c2334a8a14720cbd37
jakobgerstenlauer <jakob.gerstenlauer@est.fib.upc.edu>
2017-12-07 11:51:08 +0100

    Modification in calculateGradient: use bias trick advocated by Cristiani and add a bias dimension to the feature space.

--- src/main/scala/SVM/KernelMatrices.scala
+++ src/main/scala/SVM/KernelMatrices.scala
@@ -64,16 +64,31 @@ private def initKernelMatrixTraining() : CoordinateMatrix  = {
 }
 
 /**
- * Calculate the gradient without storing the kernel matrix Q: 
+ * Calculates the gradient vector without storing the kernel matrix Q
+ *
  * In matrix notation: Q * lambda - 1
  * For an indivual entry i of the gradient vector, this is equivalent to:
- * sum over j from 1 to N of lamba(j) * d(i) * k(i,j,) * d(j) - 1
+ * sum over j from 1 to N of lamba(j) * d(i) * k(i,j,) * d(j) 
+ * In order to avoid the constraints associated to the bias term,
+ * I use a trick advocated in Christiani & Shawe-Taylor ""An Introduction to Support Vector Machines and other kernel-based learning methods"
+ * 2000, pages 129-135: I add one dimension to the feature space that accounts for the bias. 
+ * In input space I replace x = (x1,x2,...,xn) by x_ = (x1,x2,...,xn,tau) and w = (w1,w2,...,wn) by w_ = (w1,w2,...,wn,b/tau).
+ * The SVM model <w,x>+b is then replaced by <w_,x_> = x1*w1 + x2*w2 +...+ xn*wn + tau * (b/tau).
+ * In the dual formulation, I replace K(x,y) by K(x,y) + tau * tau .
+ * This is not without drawbacks, because the "geometric margin of the separating hyperplane in the augmented space will typically be less
+ * than that in the original space." (Christiani & Shawe-Taylor, page 131)
+ * I thus have to find a suitable value of tau, which is given by the maximum of the squared euclidean norm of all inputs.
+ * With this choice, it is guaranteed that the fat-shattering dimension will not increase by more than factor 4 compared to input space.
+ *  
+ * alphas: the current dual variables
+ * tau: the a priori chosen bias term in feature space 
  * */
-def calculateGradient(alphas : DenseVector[Double]) : DenseVector[Double]  = {
+def calculateGradient(alphas : DenseVector[Double], tau : Double) : DenseVector[Double]  = {
+  val tauSquare = tau * tau
   val N = d.getN_train()
   var v = DenseVector.fill(N){-1.0}
   for (i <- 0 until N; j <- 0 until N){
-    v(i) += alphas(j) * d.z_train(i) * d.z_train(j) * kf.kernel(d.X_train(i,::).t, d.X_train(j,::).t)
+    v(i) += alphas(j) * d.z_train(i) * d.z_train(j) * (kf.kernel(d.X_train(i,::).t, d.X_train(j,::).t) + tauSquare)
   }
   v
 }
