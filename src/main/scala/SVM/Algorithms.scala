e71713e0625dc6a0c4ecf6b3333291531e7d487a
jakobgerstenlauer <jakob.gerstenlauer@est.fib.upc.edu>
2017-12-29 18:50:14 +0100

    Minor changes in format of log statements when fitting the algorithm. Signed-off-by: jakobgerstenlauer <jakob.gerstenlauer@est.fib.upc.edu>

--- src/main/scala/SVM/Algorithms.scala
+++ src/main/scala/SVM/Algorithms.scala
@@ -29,7 +29,7 @@ case class SGLocal(alphas: Alphas, ap: AlgoParams, mp: ModelParams, kmf: LocalKe
     val (correct, misclassified) = calculateAccuracy(evaluateOnTrainingSet(alphas, ap, kmf), kmf.getData().getLabelsTrain)
     val (correctT, misclassifiedT) = calculateAccuracy(evaluateOnTestSet(alphas, ap, kmf), kmf.getData().getLabelsTest)
     val sparsity = 1.0 - alphas.alpha.map(x=>if (x>0) 1 else 0).reduce(_+_).toDouble / alphas.alpha.length.toDouble
-    println("Train: "+ correct +"/"+ misclassified + " ,Test: "+ correctT + "/" + misclassifiedT+ ",Sparsity: "+ sparsity)
+    println("Train: "+ correct +"/"+ misclassified + ", Test: "+ correctT + "/" + misclassifiedT+ ", Sparsity: "+ sparsity)
 
     //Decrease the step size, i.e. learning rate:
     val ump = mp.updateDelta(ap)
@@ -57,7 +57,7 @@ case class SG(alphas: Alphas, ap: AlgoParams, mp: ModelParams, kmf: KernelMatrix
     val (correct, misclassified) = calculateAccuracy(evaluateOnTrainingSet(alphas, ap, kmf, matOps), kmf.getData().getLabelsTrain)
     val (correctT, misclassifiedT) = calculateAccuracy(evaluateOnTestSet(alphas, ap, kmf, matOps), kmf.getData().getLabelsTest)
     val sparsity = 1.0 - alphas.alpha.map(x=>if (x>0) 1 else 0).reduce(_+_).toDouble / alphas.alpha.length.toDouble
-    println("Train: "+ correct +"/"+ misclassified + " ,Test: "+ correctT + "/" + misclassifiedT+ ",Sparsity: "+ sparsity)
+    println("Train: "+ correct +"/"+ misclassified + ", Test: "+ correctT + "/" + misclassifiedT+ ", Sparsity: "+ sparsity)
 
 		//Decrease the step size, i.e. learning rate:
 		val ump = mp.updateDelta(ap)
@@ -84,7 +84,7 @@ case class SGD(alphas: Alphas, ap: AlgoParams, mp: ModelParams, kmf: KernelMatri
     val (correct, misclassified) = calculateAccuracy(evaluateOnTrainingSet(alphas, ap, kmf, matOps), kmf.getData().getLabelsTrain)
     val (correctT, misclassifiedT) = calculateAccuracy(evaluateOnTestSet(alphas, ap, kmf, matOps), kmf.getData().getLabelsTest)
     val sparsity = 1.0 - alphas.alpha.map(x=>if (x>0) 1 else 0).reduce(_+_).toDouble / alphas.alpha.length.toDouble
-    println("Train: "+ correct +"/"+ misclassified + " ,Test: "+ correctT + "/" + misclassifiedT+ ",Sparsity: "+ sparsity)
+    println("Train: "+ correct +"/"+ misclassified + ", Test: "+ correctT + "/" + misclassifiedT+ ", Sparsity: "+ sparsity)
 
     //Decrease the step size, i.e. learning rate:
     val ump = mp.updateDelta(ap)
