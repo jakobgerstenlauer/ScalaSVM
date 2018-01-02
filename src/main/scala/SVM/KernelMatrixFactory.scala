446c4848f53c6609406751c81cc2c5ce5ce2de53
jakobgerstenlauer <jakob.gerstenlauer@est.fib.upc.edu>
2018-01-02 14:08:34 +0100

    Code refactoring: New abstraction BaseMatrixFactoryWithMatrices. Signed-off-by: jakobgerstenlauer <jakob.gerstenlauer@est.fib.upc.edu>

--- src/main/scala/SVM/KernelMatrixFactory.scala
+++ src/main/scala/SVM/KernelMatrixFactory.scala
@@ -205,44 +205,5 @@ case class LeanMatrixFactory(d: Data, kf: KernelFunction, epsilon: Double) exten
   }
 }
 
-case class KernelMatrixFactory(d: Data, kf: KernelFunction, epsilon: Double, sc: SparkContext) extends BaseMatrixFactory(d, kf, epsilon) {
 
-  val matOps = new DistributedMatrixOps(sc)
-  val K : CoordinateMatrix = initKernelMatrixTraining()
-  val S : CoordinateMatrix = initKernelMatrixTest()
-  val z : DenseVector[Int] = initTargetTraining()
-  val z_test : DenseVector[Int] = initTargetTest()
-
-  private def initTargetTraining() : DenseVector[Int] = {
-    assert(d.isDefined, "The input data is not defined!")
-    d.getLabelsTrain
-  }
-
-  private def initTargetTest() : DenseVector[Int] = {
-    assert(d.isDefined, "The input data is not defined!")
-    d.getLabelsTest
-  }
-
-  private def initKernelMatrixTraining() : CoordinateMatrix  = {
-    assert(d.isDefined, "The input data is not defined!")
-    val listOfMatrixEntries =  for (i <- 0 until d.getN_train; j <- 0 until d.getN_train;
-                                    value = kf.kernel(d.getRowTrain(i), d.getRowTrain(j))
-                                    if value > epsilon) yield MatrixEntry(i, j, value)
-    // Create an RDD of matrix entries ignoring all matrix entries which are smaller than epsilon.
-    val entries: RDD[MatrixEntry] = sc.parallelize(listOfMatrixEntries)
-    new CoordinateMatrix(entries, d.getN_train, d.getN_train)
-  }
-
- private def initKernelMatrixTest() : CoordinateMatrix = {
-    assert(d.isDefined, "The input data is not defined!")
-    val listOfMatrixEntries =  for (i <- 0 until d.getN_train; j <- 0 until d.getN_test;
-                                    value = kf.kernel(d.getRowTrain(i), d.getRowTest(j))
-                                    if value > epsilon) yield MatrixEntry(i, j, value)
-    // Create an RDD of matrix entries ignoring all matrix entries which are smaller than epsilon.
-    val entries: RDD[MatrixEntry] = sc.parallelize(listOfMatrixEntries)
-    new CoordinateMatrix(entries, d.getN_train, d.getN_test)
-  }
-
-  override def getData (): Data = d
-}
 
--- src/main/scala/SVM/LocalKernelMatrixFactory.scala
+++ src/main/scala/SVM/LocalKernelMatrixFactory.scala
@@ -1,6 +1,9 @@
 package SVM
 
 import breeze.linalg._
+import org.apache.spark.SparkContext
+import org.apache.spark.mllib.linalg.distributed.{CoordinateMatrix, MatrixEntry}
+import org.apache.spark.rdd.RDD
 
 trait MatrixFactory{
   def calculateGradient(alpha: DenseVector[Double]):DenseVector[Double]
@@ -9,8 +12,7 @@ trait MatrixFactory{
   def predictOnTestSet(alphas : DenseVector[Double]):DenseVector[Double]
 }
 
-case class LocalKernelMatrixFactory(d: Data, kf: KernelFunction, epsilon: Double) extends BaseMatrixFactory(d, kf, epsilon){
-
+abstract class BaseMatrixFactoryWithMatrices(d: Data, kf: KernelFunction, epsilon: Double) extends BaseMatrixFactory(d, kf, epsilon){
   val K : DenseMatrix[Double] = initKernelMatrixTraining()
   val S : DenseMatrix[Double] = initKernelMatrixTest()
   val z : DenseVector[Int] = initTargetTraining()
@@ -26,14 +28,44 @@ case class LocalKernelMatrixFactory(d: Data, kf: KernelFunction, epsilon: Double
     d.getLabelsTest
   }
 
-  private def initKernelMatrixTraining() : DenseMatrix[Double]  = {
+  def initKernelMatrixTraining() : DenseMatrix[Double]
+  def initKernelMatrixTest() : DenseMatrix[Double]
+
+}
+
+case class KernelMatrixFactory(d: Data, kf: KernelFunction, epsilon: Double, sc: SparkContext) extends BaseMatrixFactoryWithMatrices(d, kf, epsilon) {
+
+  def initKernelMatrixTraining() : CoordinateMatrix  = {
+    assert(d.isDefined, "The input data is not defined!")
+    val listOfMatrixEntries =  for (i <- 0 until d.getN_train; j <- 0 until d.getN_train;
+                                    value = kf.kernel(d.getRowTrain(i), d.getRowTrain(j))
+                                    if value > epsilon) yield MatrixEntry(i, j, value)
+    // Create an RDD of matrix entries ignoring all matrix entries which are smaller than epsilon.
+    val entries: RDD[MatrixEntry] = sc.parallelize(listOfMatrixEntries)
+    new CoordinateMatrix(entries, d.getN_train, d.getN_train)
+  }
+
+  def initKernelMatrixTest() : CoordinateMatrix = {
+    assert(d.isDefined, "The input data is not defined!")
+    val listOfMatrixEntries =  for (i <- 0 until d.getN_train; j <- 0 until d.getN_test;
+                                    value = kf.kernel(d.getRowTrain(i), d.getRowTest(j))
+                                    if value > epsilon) yield MatrixEntry(i, j, value)
+    // Create an RDD of matrix entries ignoring all matrix entries which are smaller than epsilon.
+    val entries: RDD[MatrixEntry] = sc.parallelize(listOfMatrixEntries)
+    new CoordinateMatrix(entries, d.getN_train, d.getN_test)
+  }
+}
+
+case class LocalKernelMatrixFactory(d: Data, kf: KernelFunction, epsilon: Double) extends BaseMatrixFactoryWithMatrices(d, kf, epsilon){
+
+  def initKernelMatrixTraining() : DenseMatrix[Double]  = {
     assert(d.isDefined, "The input data is not defined!")
     val K : DenseMatrix[Double] = DenseMatrix.zeros[Double](d.getN_train, d.getN_train)
     for (i <- 0 until d.getN_train; j <- 0 until d.getN_train; value = kf.kernel(d.getRowTrain(i), d.getRowTrain(j))) K(i, j)=value
     K
   }
 
-  private def initKernelMatrixTest() : DenseMatrix[Double]  = {
+  def initKernelMatrixTest() : DenseMatrix[Double]  = {
     assert(d.isDefined, "The input data is not defined!")
     val K : DenseMatrix[Double] = DenseMatrix.zeros[Double](d.getN_train, d.getN_test)
     for (i <- 0 until d.getN_train; j <- 0 until d.getN_test; value = kf.kernel(d.getRowTrain(i), d.getRowTest(j))) K(i, j)=value
