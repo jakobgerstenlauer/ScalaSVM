90a9dc42f519d529df75e2a635716c954e136cb9
jakobgerstenlauer <jakob.gerstenlauer@est.fib.upc.edu>
2018-01-08 11:13:41 +0100

    Removed empty test class. Signed-off-by: jakobgerstenlauer <jakob.gerstenlauer@est.fib.upc.edu>

---
+++ src/main/scala/SVM/AlphasTest.scala
@@ -0,0 +1,5 @@
+package SVM
+
+class AlphasTest {
+
+}
--- src/test/scala/SVM/TestAlphas.scala
+++ src/test/scala/SVM/TestAlphas.scala
@@ -4,11 +4,16 @@ import breeze.linalg._
 import breeze.numerics._
 
 object testAlphas extends App {
-	val alpha = new DenseVector(Array(1.0,1.0))
-	val alpha_old = new DenseVector(Array(-1.0,1.0))
-	val alphas = Alphas(2, alpha, alpha_old)
-	println("alpha_old: "+alphas.alphaOld)
-	println("alpha before update : "+alphas.alpha)
-	alphas.updateAlphaAsConjugateGradient()
-	println("alpha after update: "+alphas.alpha)
+	val N = 1000
+	val alpha : DenseVector[Double] = DenseVector.rand(N)
+	val alpha_old : DenseVector[Double] = DenseVector.fill(N){0.0}
+	val alphas = Alphas(N, alpha, alpha_old)
+	println("alphas: "+alphas.alpha)
+	println("10% Quantile: "+alphas.getQuantile(0.1))
+	println("20% Quantile: "+alphas.getQuantile(0.2))
+	println("Median: "+alphas.getQuantile(0.5))
+	println("90% Quantile: "+alphas.getQuantile(0.9))
+	//println("alpha before update : "+alphas.alpha)
+	//alphas.updateAlphaAsConjugateGradient()
+	//println("alpha after update: "+alphas.alpha)
 }
