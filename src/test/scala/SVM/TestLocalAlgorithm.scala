82882dfcbe51ec2bbb30ae133749f3d26c26cebc
jakobgerstenlauer <jakob.gerstenlauer@est.fib.upc.edu>
2018-01-08 16:25:49 +0100

    new test class

--- src/test/scala/SVM/TestLocalAlgorithm.scala
+++ src/test/scala/SVM/TestLocalAlgorithm.scala
@@ -18,12 +18,12 @@ object TestLocalAlgorithm extends App {
   val epsilon = 0.0001
   val kernelPar = GaussianKernelParameter(10.0)
   val gaussianKernel = GaussianKernel(kernelPar)
-  val kmf = LocalKernelMatrixFactory(d, gaussianKernel, epsilon)
+  val kmf = LeanMatrixFactory(d, gaussianKernel, epsilon)
   val mp = ModelParams(C = 0.1, delta = 0.3)
   val alphas = new Alphas(N=d.N_train, mp)
   val ap = AlgoParams(maxIter = 5, batchProb = 0.8, minDeltaAlpha = 0.001, learningRateDecline = 0.9,
     epsilon = epsilon, isDebug = true, hasMomentum = false)
-  var algo1 = SGLocal(alphas, ap, mp, kmf)
+  var algo1 = NoMatrices(alphas, ap, mp, kmf)
   var numInt = 0
   while(numInt < 2){
     algo1 = algo1.iterate()
