76e70b425f92594438a38dc64e933ac119999d77
jakobgerstenlauer <jakob.gerstenlauer@est.fib.upc.edu>
2018-01-11 14:57:38 +0100

    Add new tests to AlphasTest. Signed-off-by: jakobgerstenlauer <jakob.gerstenlauer@est.fib.upc.edu>

--- src/test/scala/SVM/AlphasTest.scala
+++ src/test/scala/SVM/AlphasTest.scala
@@ -5,7 +5,29 @@ import breeze.linalg._
 
 class AlphasTest extends FunSuite{
 
-  test("First element in sorted array must be 1."){
+  test("After clipping 50% quantile, sparsity must be 50%."){
+    val N = 10
+    val alpha : DenseVector[Double] = new DenseVector(Array(2.0,1.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0))
+    val alpha_old : DenseVector[Double] = DenseVector.fill(N){0.0}
+    val alphas = Alphas(N, alpha, alpha_old)
+    val alphasClipped = alphas.clipAlphas(0.5)
+    val sumNonSparse = alphasClipped.alpha.map(x=>if(x>0) 1 else 0 ).reduce(_ + _)
+    assert(sumNonSparse == 5)
+  }
+
+  test("getDelta() must return 10."){
+    val N = 10
+    val alpha : DenseVector[Double] = DenseVector.fill(N){1.0}
+    val alpha_old : DenseVector[Double] = DenseVector.fill(N){0.0}
+    val alphas = Alphas(N, alpha, alpha_old)
+    assert(alphas.getDelta == 10.0)
+  }
+
+  test("Mean must be 5.0."){
+    assert(Alphas.mean(0.0,10.0) == 5.0)
+  }
+
+  test("First element in sorted array must be 1 and the original vector must not change."){
     val N = 10
     val alpha : DenseVector[Double] = new DenseVector(Array(2.0,1.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0))
     val alpha_old : DenseVector[Double] = DenseVector.fill(N){0.0}
