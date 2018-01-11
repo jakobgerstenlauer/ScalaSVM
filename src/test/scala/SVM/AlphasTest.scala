9b3c87c22081a2b3654df87863a3b42f3f5fa1a0
jakobgerstenlauer <jakob.gerstenlauer@est.fib.upc.edu>
2018-01-11 14:41:20 +0100

    New test for Alphas. Signed-off-by: jakobgerstenlauer <jakob.gerstenlauer@est.fib.upc.edu>

--- src/test/scala/SVM/AlphasTest.scala
+++ src/test/scala/SVM/AlphasTest.scala
@@ -5,6 +5,18 @@ import breeze.linalg._
 
 class AlphasTest extends FunSuite{
 
+  test("First element in sorted array must be 1."){
+    val N = 10
+    val alpha : DenseVector[Double] = new DenseVector(Array(2.0,1.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0))
+    val alpha_old : DenseVector[Double] = DenseVector.fill(N){0.0}
+    val alphas = Alphas(N, alpha, alpha_old)
+    val sorted = alphas.getSortedAlphas
+    //the returned array must contain 1.0 at position 0!
+    assert(sorted(0) == 1.0)
+    //the original vector must not be changed!
+    assert(alphas.alpha(0) == 2.0)
+  }
+
   test("Median must be 5.5."){	
 	val N = 10
 	val alpha : DenseVector[Double] = new DenseVector(Array(1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0))
