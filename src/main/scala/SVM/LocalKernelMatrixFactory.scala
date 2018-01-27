d9b9372183ea20e9a0dbd7c1a5a4bbd20d420bfe
jakobgerstenlauer <jakob.gerstenlauer@est.fib.upc.edu>
2018-01-27 15:04:54 +0100

    evaluate() with additional log statements for debugging.

--- src/main/scala/SVM/LocalKernelMatrixFactory.scala
+++ src/main/scala/SVM/LocalKernelMatrixFactory.scala
@@ -65,9 +65,11 @@ case class KernelMatrixFactory(d: Data, kf: KernelFunction, epsilon: Double, sc:
   def evaluate(alphas: Alphas, ap: AlgoParams, kmf: KernelMatrixFactory, matOps: DistributedMatrixOps, dataType: SVM.DataSetType.Value):DenseVector[Double]= {
     //Get the distributed kernel matrix for the given typ of data set:
     val K : CoordinateMatrix = kmf.getKernelMatrix(dataType)
+    println("Kernel matrix with rows: "+K.numRows()+ "and columns: "+K.numCols() + ".")
     assert(K.numCols()>0, "The number of columns of the kernel matrix is zero.")
     assert(K.numRows()>0, "The number of rows of the kernel matrix is zero.")
     val z = kmf.z.map(x=>x.toDouble)
+    println("Label vector z with length: "+z.length + ".")
     assert(K.numRows()==z.length,"The number of rows "+K.numCols()
       +"of the kernel matrix does not equal the length "+z.length+" of the vector of labels!")
     val epsilon = max(min(ap.epsilon, min(alphas.alpha)), 0.000001)
@@ -77,7 +79,10 @@ case class KernelMatrixFactory(d: Data, kf: KernelFunction, epsilon: Double, sc:
     assert(A.numRows()>0, "The number of rows of A is zero.")
     assert(A.numCols()==K.numRows(),"The number of columns of A does not equal the number of rows of the kernel matrix!")
     val P = matOps.coordinateMatrixMultiply(A, K)
+    println("Result matrix with rows: "+P.numRows()+ "and columns: "+P.numCols() + ".")
     //Return the predictions
-    signum(matOps.collectRowVector(P))
+    val predictions = signum(matOps.collectRowVector(P))
+    println("Predictions vector with length: "+predictions.length + ".")
+    predictions
   }
 }
